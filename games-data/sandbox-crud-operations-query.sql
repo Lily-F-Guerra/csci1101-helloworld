aa0045500248957d0730003427ef7146dc5093f4
[Lily-F-Guerra] <[lf_guerra3@yahoo.com]>
2022-11-22 13:22:03 -0600

    fun with databases

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+--INSERT INTO sandbox(int64_value, bool_value, string_value) VALUES(122712271227, 1, 'have a beautiful afternoon');
+
+--UPDATE sandbox SET string_value = 'motorcycle chickens' WHERE id=10;
+
+--DELETE FROM sandbox WHERE id=161;
+
+select * from sandbox;
